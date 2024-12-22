cc3930abd101e3f58dc0916eb143446bba74706e
MossDog <lukejohnmosshughes@gmail.com>
2024-12-22 02:40:05 +0000

    images loaded from webserver

--- helpful_stuff.md
+++ helpful_stuff.md
@@ -9,3 +9,7 @@ npx tailwindcss -i ./input.css -o ./style.css --watch
  - [DaisyUI Components](https://daisyui.com/components/)
  - [Tailwind Components](https://tailwindui.com/components?ref=sidebar)
  - [Domus Lucae](https://MossDog.github.io)
+
+# Plans
+ - Have recent updates be displayed on the landing page, dynamically loaded from database
+ - Reactive design for mobile
--- index.js
+++ index.js
@@ -2,13 +2,18 @@ console.log("Hello There")
 
 const themeSwitch = document.getElementById("theme-switch");
 const logo = document.getElementById("logo");
+const imgSrc = "http://ec2-54-144-213-152.compute-1.amazonaws.com:3000/public/images/"
 
 // Define the logo assets for light and dark modes
-const lightLogo = "Assets/logoLightCroppedSmall1.png";
-const darkLogo = "Assets/logoDarkCroppedSmall1.png";
+
 
 // Event listener to toggle the logo on theme switch
 themeSwitch.addEventListener("change", () => {
+  if(themeSwitch.checked){
+    logo.src = imgSrc + "/logoDarkCroppedSmall1.png"
+  }else{
+    logo.src = imgSrc + "logoLightCroppedSmall1.png"
+  }
   logo.src = themeSwitch.checked ? darkLogo : lightLogo;
 });
 
